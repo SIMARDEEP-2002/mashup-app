5c83a23ffaecb89fbbdbf6cdef6ddd342bc5dc47
SIMARDEEP-2002 <79918370+SIMARDEEP-2002@users.noreply.github.com>
2023-02-11 23:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ with form:
             # flag2=0
             
     else:
-        st.write("All fields are required. If all fields are filled check for errors.\n Wait till email sent message before colsing the app!")
+        st.write("All fields are required. If all fields are filled check for errors. Wait till email sent message before colsing the app!")
+        st.write("Wait till email sent message before colsing the app!)
         
     # if flag2==1:
     #     import shutil
